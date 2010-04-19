0a1e06f2641573670b1a4c91d186f24b382725da
apondman <apondman@c08c062a-ad99-a35b-71ae-6f72dbcc447f>
2010-04-19 18:10:04 +0000

    initial commit of BDHandler 0.9.0 source

---
+++ Src/BDHandler/BDHandlerPlugin.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using MediaPortal.Configuration;
+using MediaPortal.GUI.Library;
+using MediaPortal.Player;
+
+namespace MediaPortal.Plugins.BDHandler {
+
+    [PluginIcons("MediaPortal.Plugins.BDHandler.Resources.BDHandler.png", "MediaPortal.Plugins.BDHandler.Resources.BDHandlerDisabled.png")]
+    public class BDHandlerPlugin : IPlugin, ISetupForm {
+
+        public static string LogPrefix = "[BDHandler] ";
+        private FactoryWrapper _factory;
+
+        public BDHandlerPlugin() {
+            Assembly assy = Assembly.GetExecutingAssembly();
+            foreach (Attribute attr in Attribute.GetCustomAttributes(assy)) {
+                if (attr.GetType() == typeof(AssemblyTitleAttribute))
+                    _pluginName = ((AssemblyTitleAttribute)attr).Title;
+                else if (attr.GetType() == typeof(AssemblyDescriptionAttribute))
+                    _pluginDesc = ((AssemblyDescriptionAttribute)attr).Description;
+                else if (attr.GetType() == typeof(AssemblyCompanyAttribute))
+                    _pluginAuthor = ((AssemblyCompanyAttribute)attr).Company;
+            }
+        }
+
+
+        #region IPlugin Members
+
+        public void Start() {
+            _factory = new FactoryWrapper(g_Player.Factory);
+            g_Player.Factory = _factory;
+            Log.Info(LogPrefix + " Enabled.");
+        }
+
+        public void Stop() {
+            g_Player.Factory = _factory.GetDefaultFactory();
+            Log.Info(LogPrefix + " Disabled.");
+        }
+
+        #endregion
+
+        #region ISetupForm Members
+
+        public string PluginName() {
+            return _pluginName;
+        } private string _pluginName;
+
+        public string Description() {
+            return _pluginDesc; 
+        } private string _pluginDesc;
+
+        public string Author() {
+            return _pluginAuthor;
+        }  private string _pluginAuthor;
+
+        public void ShowPlugin() {
+            return;
+        }
+
+        public bool CanEnable() {
+            return true;
+        }
+
+        public int GetWindowId() {
+            return 0;
+        }
+
+        public bool DefaultEnabled() {
+            return true;
+        }
+
+        public bool HasSetup() {
+            return false;
+        }
+
+        public bool GetHome(out string strButtonText, out string strButtonImage, out string strButtonImageFocus, out string strPictureImage) {
+            strButtonText = string.Empty;
+            strButtonImage = string.Empty;
+            strButtonImageFocus = string.Empty;
+            strPictureImage = string.Empty;
+            return false;
+        }
+
+        #endregion
+    }
+
+}
---
+++ Src/BDHandler/Player/BDPlayer.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DirectShowLib;
+using DShowNET.Helper;
+using MediaPortal.Configuration;
+using MediaPortal.Player;
+using MediaPortal.GUI.Library;
+using MediaPortal.Profile;
+using MediaPortal.Player.Subtitles;
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Xml;
+using System.Xml.XPath;
+using System.Threading;
+
+using BDInfo;
+
+namespace MediaPortal.Plugins.BDHandler {
+
+    public class BDPlayer : VideoPlayerVMR9
+    {
+
+        // "MPC - Mpeg Source (Gabest)
+        private static Guid MpcMpegSourceFilter {
+            get {
+                return new Guid("{1365BE7A-C86A-473C-9A41-C0A6E82C9FA3}");
+            }
+        }
+        
+        private static string MpcMegSourceFilterName = "MPC - Mpeg Source (Gabest)";
+
+        public BDPlayer() : base(g_Player.MediaType.Video) { }
+
+        public BDPlayer(g_Player.MediaType type) : base(type)
+        { }
+
+        public override bool Play(string strFile) {
+            string path = strFile.ToLower();
+            
+            if (path.Length < 4) {
+                string discPath = Path.Combine(path,@"BDMV\index.bdmv");
+                if (File.Exists(discPath))
+                    path = discPath;
+            }
+            
+            if (path.EndsWith(".bdmv") || path.EndsWith(".m2ts"))
+                strFile = doFeatureSelection(path);
+
+            return base.Play(strFile);
+        }
+
+        delegate BDROM ScanProcess(string path);
+ 
+        private BDROM scanWorker(string path) {
+            Log.Info(BDHandlerPlugin.LogPrefix + "Scanning bluray structure for request: {0}", path);
+            BDROM bluray = new BDROM(path.ToUpper());
+            bluray.Scan();
+            return bluray;            
+        }
+
+        private string doFeatureSelection(string path) {
+            try {
+
+                ScanProcess scanner = new ScanProcess(scanWorker);
+                IAsyncResult result = scanner.BeginInvoke(path, null, scanner);
+
+                // Show the wait cursor during scan
+                GUIWaitCursor.Init();
+                GUIWaitCursor.Show(); 
+                while(result.IsCompleted == false) {
+                    GUIWindowManager.Process();
+                    Thread.Sleep(100);
+                }
+
+                BDROM bluray = scanner.EndInvoke(result);
+                List<TSPlaylistFile> playLists = bluray.PlaylistFiles.Values.Where(p => p.IsValid).OrderByDescending(p => p.TotalLength).ToList();
+
+                GUIWaitCursor.Hide();
+
+                if (playLists.Count == 0)
+                    return path;
+
+                if (playLists.Count == 1)
+                    return Path.Combine(bluray.DirectoryPLAYLIST.FullName, playLists[0].Name);
+
+                IDialogbox dialog = (IDialogbox)GUIWindowManager.GetWindow((int)GUIWindow.Window.WINDOW_DIALOG_MENU);
+                dialog.Reset();
+
+                for (int i = 0; i < playLists.Count; i++) {
+                    TSPlaylistFile playList = playLists[i];
+                    TimeSpan lengthSpan = new TimeSpan((long)(playList.TotalLength * 10000000));
+                    string length = string.Format("{0:D2}:{1:D2}:{2:D2}", lengthSpan.Hours, lengthSpan.Minutes, lengthSpan.Seconds);
+                    string feature = string.Format("Feature #{0} ({1})", (i + 1), length);
+                    dialog.Add(feature);
+                }
+
+                dialog.SetHeading("Bluray: Select Feature");
+                dialog.DoModal(GUIWindowManager.ActiveWindow);
+                if (dialog.SelectedId < 1)
+                    return path;
+
+                return Path.Combine(bluray.DirectoryPLAYLIST.FullName, playLists[dialog.SelectedId - 1].Name);
+            }
+            catch (Exception e) {
+                Log.Error(BDHandlerPlugin.LogPrefix + "Exception while reading bluray structure {0} {1}", e.Message, e.StackTrace);
+                return path;
+            }
+        }
+
+        protected override bool GetInterfaces()
+        {
+            if (CurrentFile.ToLower().EndsWith(".mpls"))
+                return renderGraph();        
+            
+            return base.GetInterfaces();
+        }
+
+        private bool renderGraph() {
+            try {
+                graphBuilder = (IGraphBuilder)new FilterGraph();
+                _rotEntry = new DsROTEntry((IFilterGraph)graphBuilder);
+
+                Log.Info("BDPlayer: Active");
+                GUIMessage msg = new GUIMessage(GUIMessage.MessageType.GUI_MSG_SWITCH_FULL_WINDOWED, 0, 0, 0, 1, 0, null);
+                GUIWindowManager.SendMessage(msg);
+
+                Vmr9 = new VMR9Util();
+                Vmr9.AddVMR9(graphBuilder);
+                Vmr9.Enable(false);
+
+                // load the source filter                
+                IBaseFilter source = DirectShowUtil.AddFilterToGraph(graphBuilder, MpcMegSourceFilterName);
+
+                // check if it's avaiable
+                if (source == null) {
+                    Error.SetError("Unable to load source filter", "Please register filter: " + MpcMegSourceFilterName);
+                    Log.Error(BDHandlerPlugin.LogPrefix + "Unable to load DirectShowFilter: " + MpcMegSourceFilterName, null);
+                    return false;
+                }
+
+                // load the file
+                int result = ((IFileSourceFilter)source).Load(CurrentFile, null);
+                if (result != 0) return false;
+               
+                // add filters and audio renderer
+                using (Settings settings = new Settings(Config.GetFile(Config.Dir.Config, "MediaPortal.xml"))) {
+
+                    string strVideoCodec = settings.GetValueAsString("movieplayer", "mpeg2videocodec", "");
+                    string strH264VideoCodec = settings.GetValueAsString("movieplayer", "h264videocodec", "");
+                    string strAudioCodec = settings.GetValueAsString("movieplayer", "mpeg2audiocodec", "");
+                    string strAACAudioCodec = settings.GetValueAsString("movieplayer", "aacaudiocodec", "");
+                    string strAudiorenderer = settings.GetValueAsString("movieplayer", "audiorenderer", "Default DirectSound Device");
+
+                    if (!string.IsNullOrEmpty(strH264VideoCodec))
+                        DirectShowUtil.AddFilterToGraph(graphBuilder, strH264VideoCodec);
+                    if (!string.IsNullOrEmpty(strVideoCodec) && strVideoCodec != strH264VideoCodec)
+                        DirectShowUtil.AddFilterToGraph(graphBuilder, strVideoCodec);
+                    if (!string.IsNullOrEmpty(strAudioCodec))
+                        DirectShowUtil.AddFilterToGraph(graphBuilder, strAudioCodec);
+                    if (!string.IsNullOrEmpty(strAACAudioCodec) && strAudioCodec != strAACAudioCodec)
+                        DirectShowUtil.AddFilterToGraph(graphBuilder, strAACAudioCodec);                    
+                    
+                    DirectShowUtil.AddAudioRendererToGraph(graphBuilder, strAudiorenderer, false);
+                }
+
+                DirectShowUtil.RenderUnconnectedOutputPins(graphBuilder, source);
+                DirectShowUtil.ReleaseComObject(source); source = null;
+                DirectShowUtil.RemoveUnusedFiltersFromGraph(graphBuilder);
+                
+                SubEngine.GetInstance().LoadSubtitles(graphBuilder, m_strCurrentFile);
+
+                if (Vmr9 == null || !Vmr9.IsVMR9Connected) {
+                    Log.Error(BDHandlerPlugin.LogPrefix + "Failed to render file.");
+                    mediaCtrl = null;
+                    Cleanup();
+                    return false;
+                }
+
+                mediaCtrl = (IMediaControl)graphBuilder;
+                mediaEvt = (IMediaEventEx)graphBuilder;
+                mediaSeek = (IMediaSeeking)graphBuilder;
+                mediaPos = (IMediaPosition)graphBuilder;
+                basicAudio = (IBasicAudio)graphBuilder;
+                videoWin = (IVideoWindow)graphBuilder;
+                m_iVideoWidth = Vmr9.VideoWidth;
+                m_iVideoHeight = Vmr9.VideoHeight;
+                Vmr9.SetDeinterlaceMode();
+                return true;
+            }
+            catch (Exception e) {
+                Error.SetError("Unable to play movie", "Unable build graph for VMR9");
+                Log.Error(BDHandlerPlugin.LogPrefix + "Exception while creating DShow graph {0} {1}", e.Message, e.StackTrace);
+                Cleanup();
+                return false;
+            }
+        }
+
+    }
+}
