4967cf0fc7d2e43d0d41c0b942a0d25cd6388315
apondman <apondman@c08c062a-ad99-a35b-71ae-6f72dbcc447f>
2010-04-19 18:10:04 +0000

    initial commit of BDHandler 0.9.0 source

---
+++ trunk/Src/BDHandler/BDROM/BDInfoSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BDInfo {
+    public class BDInfoSettings {
+    
+        public static bool FilterLoopingPlaylists = true;
+        public static bool FilterShortPlaylists = true;
+        public static int FilterShortPlaylistsValue = 3600;
+        public static bool KeepStreamOrder = false;
+
+    }
+}
---
+++ trunk/Src/BDHandler/Player/FactoryWrapper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaPortal.Player;
+using MediaPortal.GUI.Library;
+
+namespace MediaPortal.Plugins.BDHandler {
+    
+    public class FactoryWrapper : IPlayerFactory {
+        
+        IPlayerFactory _defaultFactory;
+
+        public FactoryWrapper(IPlayerFactory factory) {
+            this._defaultFactory = factory;
+        }
+
+        public IPlayerFactory GetDefaultFactory() {
+            return _defaultFactory;
+        }
+
+        public IPlayer Create(string filename) {
+            return Create(filename, g_Player.MediaType.Video);
+        }
+
+        public IPlayer Create(string filename, g_Player.MediaType type) {
+            string filepath = filename.ToLower();
+            if (filepath.EndsWith(".mpls") || filepath.EndsWith(".bdmv") || (filepath.Contains(@"bdmv\stream") && filepath.EndsWith(".m2ts"))) {
+                return new BDPlayer();
+            }
+
+            return _defaultFactory.Create(filename, type);
+        }
+
+    }
+}
